02f814d7c6a94a7f1f74f378151bcf5a01d0733d
Abinash Anand <74396889+Abinash-Anand@users.noreply.github.com>
2023-07-10 13:32:50 +0530

    dark mode feature added

--- script.js
+++ script.js
@@ -97,3 +97,102 @@ btn.forEach(function (btn) {
 //   (window.open = "https://developer.mozilla.org/en-US/docs/Web/CSS/box-shadow"),
 //     "_blank";
 // }
+// const cloudElement = document.querySelector(".top-cloud");
+const topContainer = document.querySelector(".top_container");
+// const middleContainer = document.querySelector(".middle-container");
+// const footer = document.querySelector(".foot-bar");
+// const elementArray = [topContainer, middleContainer, footer];
+
+// function darkMode() {
+//   for (let index = 0; index < elementArray.length; index++) {
+//     body.style.backgroundColor = "grey";
+//   }
+// }
+
+// cloudElement.addEventListener("click", darkMode);
+const heading = document.querySelectorAll(".heading-animation");
+const centerLineElement = document.querySelector(".center-line");
+const cloudElement = document.querySelector(".top-cloud");
+const cloudElementTwo = document.querySelector(".bottom-cloud");
+const elementArray = [document.body, topContainer]; // Add other elements you want to apply dark mode to here
+const webTechHeading = document.querySelectorAll(".web-tech");
+const techStackComponent = document.querySelectorAll(".tech-stack");
+const internshipExpContent = document.querySelectorAll(".child");
+const socialIcons = document.querySelectorAll(".socials");
+const copyRight = document.querySelector(".copy-right");
+const workExp = document.querySelector(".exp");
+const workExpChildElement = document.querySelectorAll(".child");
+const harryPotter = document.querySelector(".harry-potter");
+const technologyElements = document.querySelectorAll(".technology");
+function darkMode() {
+  for (let index = 0; index < elementArray.length; index++) {
+    //body backgeound color
+    elementArray[index].style.backgroundColor = "#000000";
+    elementArray[index].style.color = "white";
+    workExp.style.backgroundColor = "#20262E";
+    workExp.style.color = "#000000";
+    copyRight.style.paddingTop = "15px";
+    copyRight.style.paddingLeft = "50px";
+    copyRight.style.color = "#fff";
+    centerLineElement.style.backgroundColor = "white";
+    centerLineElement.style.filter =
+      "brightness(150%) drop-shadow(0 0 10px rgba(0, 255, 255, 0.7))";
+    copyRight.style.filter =
+      "brightness(150%) drop-shadow(0 0 10px rgba(0, 255, 255, 0.7))";
+    harryPotter.style.backgroundColor = "white";
+    cloudElement.style.filter =
+      " brightness(150%) drop-shadow(0 0 10px rgba(0, 255, 255, 0.7))";
+    cloudElementTwo.style.filter =
+      "brightness(150%) drop-shadow(0 0 10px rgba(0, 255, 255, 0.7))";
+    //transitions ==================
+    elementArray.forEach((transition) => {
+      transition.style.transition = ".5s ease-in";
+    });
+    //heading glowup
+    heading.forEach((element) => {
+      element.style.filter =
+        "brightness(150%) drop-shadow(0 0 10px rgba(0, 255, 255, 0.7))";
+    });
+
+    //tech stack component
+    techStackComponent.forEach((component) => {
+      component.style.backgroundColor = "#20262E";
+    });
+    //web stack technology elements components
+    technologyElements.forEach((elements) => {
+      elements.style.backgroundColor = "#3C4048";
+    });
+
+    //web stack
+    webTechHeading.forEach((heading) => {
+      heading.style.color = "white";
+    });
+
+    //stack elements
+    webTechHeading.forEach((heading) => {
+      heading.style.color = "white";
+      heading.style.filter =
+        "brightness(150%) drop-shadow(0 0 10px rgba(0, 255, 255, 0.7))";
+    });
+
+    // workExp.style.boxShadow = "15px 10px  rgb(41, 41, 41)";
+    internshipExpContent.forEach((element) => {
+      element.style.backgroundColor = "#D1D1D1";
+
+      // element.style.color = "#fff";
+    });
+    internshipExpContent;
+    //darkmode for social elements
+    socialIcons.forEach((icon) => {
+      icon.style.backgroundColor = "white";
+      icon.style.borderRadius = "50%";
+      icon.style.filter =
+        "brightness(150%) drop-shadow(0 0 10px rgba(0, 255, 255, 0.7))";
+    });
+  }
+  // for (let index = 0; index < array.length; index++) {
+  //   fontColorElements[index].color = "white";
+  // }
+}
+
+cloudElement.addEventListener("click", darkMode);
